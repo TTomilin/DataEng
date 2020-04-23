ec5333f6929f25f75d2e798708aede9385971fe0
Tristan Tomilin <tristan.tomilin@nortal.com>
2020-04-24 00:18:11 +0200

    Correlation setup

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,11 @@
+import session.SessionWrapper;
+import statistics.StatisticsManager;
+
 public class Application {
 
 	public static void main(String[] args) {
-
+		StatisticsManager manager = new StatisticsManager();
+		manager.correlation();
+		SessionWrapper.close();
 	}
 }
