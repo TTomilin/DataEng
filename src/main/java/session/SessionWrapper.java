5353ba9ff6933a595f3cc306be138b36ee8d143d
Tristan Tomilin <tristan.tomilin@nortal.com>
2020-04-24 00:13:55 +0200

    Session wrapper

---
+++ src/main/java/session/SessionWrapper.java
@@ -0,0 +1,25 @@
+package session;
+
+import javax.inject.Singleton;
+
+import org.apache.spark.sql.SparkSession;
+
+@Singleton
+public class SessionWrapper {
+
+	private static SparkSession session;
+
+	public static SparkSession getSession() {
+		if (session == null) {
+			session = SparkSession.builder()
+					.appName("DataEng")
+					.config("spark.master", "local")
+					.getOrCreate();
+		}
+		return session;
+	}
+
+	public static void close() {
+		session.stop();
+	}
+}
