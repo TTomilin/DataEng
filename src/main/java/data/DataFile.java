7c558a53445afc95667b2c613f90610728a2ad89
Tristan Tomilin <tristan.tomilin@hotmail.com>
2020-05-24 13:17:14 +0200

    Added discretized data. Shortened file names.

--- src/main/java/data/DataFile.java
+++ src/main/java/data/DataFile.java
@@ -1,17 +1,18 @@
 package data;
 
 public enum DataFile {
-	WIND	("wind_generation"),
-	SOLAR	("solar_generation"),
+	WIND	("wind"),
+	SOLAR	("solar"),
 
 	// Development
-	WIND_1000ROWS			("wind_generation_1000rows"),
-	WIND_100ROWS			("wind_generation_100rows"),
-	WIND_100ROWS_3COUNTRIES	("wind_generation_100rows_3countries"),
-	WIND_10ROWS_2COUNTRIES	("wind_generation_10rows_2countries"),
-	WIND_10ROWS_3COUNTRIES	("wind_generation_10rows_3countries"),
-	WIND_10ROWS_4COUNTRIES	("wind_generation_10rows_4countries"),
-	WIND_10ROWS_7COUNTRIES	("wind_generation_10rows_7countries");
+	WIND_1000ROWS				("wind_1000rows"),
+	WIND_100ROWS				("wind_100rows"),
+	WIND_100ROWS_3COUNTRIES		("wind_100rows_3countries"),
+	WIND_10ROWS_2COUNTRIES		("wind_10rows_2countries"),
+	WIND_10ROWS_3COUNTRIES		("wind_10rows_3countries"),
+	WIND_10ROWS_4COUNTRIES		("wind_10rows_4countries"),
+	WIND_10ROWS_7COUNTRIES		("wind_10rows_7countries"),
+	WIND_10ROWS_6COUNTRIES_DISC	("wind_10rows_6countries_disc");
 
 	private final String fileName;
 
