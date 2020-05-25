b9b8b1e9d5c92783913bff25fef56c4637dec1b9
Tristan Tomilin <tristan.tomilin@hotmail.com>
2020-05-25 17:21:16 +0200

    Printing marginal occurrences

--- src/main/java/statistics/mapper/computation/TotalStatisticComputer.java
+++ src/main/java/statistics/mapper/computation/TotalStatisticComputer.java
@@ -1,5 +1,6 @@
 package statistics.mapper.computation;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
 
@@ -19,6 +20,9 @@ public class TotalStatisticComputer implements Function<Map<Collection<Integer>,
 		int[] marginalOccurrences = new int[BIN_SIZE];
 		countMap.keySet().stream().flatMap(Collection::stream).forEach(value -> marginalOccurrences[value]++);
 		countMap.forEach((key, val) -> log(key, val));
+		for (int i = 0; i < BIN_SIZE; i++) {
+			System.out.println(i + " occurs " + marginalOccurrences[i] + " times");
+		}
 		return null;
 	}
 
